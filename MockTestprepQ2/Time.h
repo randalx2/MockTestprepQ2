01d5452e962b8c3acbf350d3d0c3ba49863a6405
randalx2 <pmisthry@gmail.com>
2016-03-15 00:53:27 +0200

    Created skelton code for main and header file

---
+++ MockTestprepQ2/Time.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef TIME_H
+#define TIME_H
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+//Declaring Class Prototype
+class Time {
+private: int hours, minutes;
+
+public: 
+};
+
+#endif
---
+++ MockTestprepQ2/main.cpp
@@ -0,0 +1,22 @@
+// ***** QUESTION 2 *****
+// Question 2 consists of 3 parts
+// 2.1  Create a class "Time" which implements a 24hr clock in the format hh:mm   
+//      Include default constructor, destructor, accessor and mutator (see how they are used in main to
+//      determine the format required.
+//      Put the declaration and definition in appropriate files! 
+// 2.2  Overload the stream insertion operator for your class to print the time in format hh:mm 
+// 2.3  Overload the POSTFIX increment operator to increment the minutes (include carry if needed)
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+
+	system("PAUSE");
+	return 0;
+}
